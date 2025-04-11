64c1190ae3096f5608fc691d317a0a49b427996d
ynzung <syj91105@sookmyung.ac.kr>
2025-04-12 00:17:31 +0900

    Design: 기본 스타일 설정 초기화

--- src/style/global.ts
+++ src/style/global.ts
@@ -2,6 +2,23 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 
+
+*{box-sizing:border-box}
+body, button, dd, dl, dt, fieldset, form, h1, h2, h3, h4, h5, h6, input, legend, li, ol, p, select, table, td, textarea, th, ul {margin:0;padding:0}
+body, button, input, select, table, textarea {font-size:14px;line-height:16px;color:#3B3B3B;font-family: "Pretendard", -apple-system, BlinkMacSystemFont, "Malgun Gothic", "맑은 고딕", helvetica, sans-serif}
+h1, h2, h3, h4, h5, h6 {font-size:inherit;line-height:inherit}
+textarea {-webkit-backface-visibility:hidden;backface-visibility:hidden;background-color:transparent;border:0;word-break:keep-all;word-wrap:break-word}
+button, input {-webkit-border-radius:0;border-radius:0;border:0}
+button {background-color:transparent}
+fieldset, img {border:0}
+img {vertical-align:top}
+ol, ul {list-style:none}
+address, em {font-style:normal}
+a {display:flex;text-decoration:none;cursor:pointer;}
+iframe {overflow:hidden;margin:0;border:0;padding:0;vertical-align:top}
+mark {background-color:transparent}
+i {font-style:normal}
+
 #root {
 	display: flex;
 	flex-direction: column;
