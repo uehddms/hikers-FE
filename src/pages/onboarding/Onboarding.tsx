ce869f9401aa291b7f52fda7aa57ae4f218481f9
ynzung <syj91105@sookmyung.ac.kr>
2025-04-13 01:24:00 +0900

    Design: 온보딩 스타일 수정

--- src/pages/onboarding/Onboarding.tsx
+++ src/pages/onboarding/Onboarding.tsx
@@ -1,4 +1,6 @@
 import { BottomBtn } from "../../components/common/button/BottomBtn";
+import Logo from "../../components/common/logo/Logo";
+import MountainLogo from "../../components/common/logo/MountainLogo";
 import { NotMemberYet } from "../../components/onboarding/NotMemberYet";
 import * as Styled from "./styled";
 import { useNavigate } from "react-router-dom";
@@ -11,10 +13,16 @@ const Onboarding = () => {
   return (
     <>
       <Styled.Wrapper>
-        <BottomBtn onClick={handleGoLogin} disabled={false}>
-          로그인
-        </BottomBtn>
-        <NotMemberYet />
+        <Styled.LogoWrapper>
+          <Logo />
+          <Styled.ButtonWrapper>
+            <MountainLogo />
+            <BottomBtn onClick={handleGoLogin} disabled={false}>
+              로그인
+            </BottomBtn>
+            <NotMemberYet />
+          </Styled.ButtonWrapper>
+        </Styled.LogoWrapper>
       </Styled.Wrapper>
     </>
   );
--- src/pages/onboarding/styled.ts
+++ src/pages/onboarding/styled.ts
@@ -1,14 +1,26 @@
 import styled from "styled-components";
-import OnboardingImg from "../../assets/Onboarding.svg";
 
 export const Wrapper = styled.div`
-  background-image: url(${OnboardingImg});
-  width: 390px;
-  height: 844px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 154px 0 33px 0;
+  align-items: center;
+  gap: 154px;
+`;
+
+export const LogoWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: flex-end;
+  gap: 156px;
+`;
+
+export const ButtonWrapper = styled.div`
+  margin-top: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   gap: 14px;
-  padding-bottom: 33px;
 `;
