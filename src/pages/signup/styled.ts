2c1b7965c5571ef837accb9704ff02482a3382e9
ynzung <syj91105@sookmyung.ac.kr>
2025-04-13 23:47:14 +0900

    Design: 회원가입 페이지 css 스타일 수정

--- src/pages/signup/styled.ts
+++ src/pages/signup/styled.ts
@@ -4,7 +4,6 @@ import greyBackground from "../../assets/greyBackground.svg";
 export const Wrapper = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
   flex-direction: column;
   width: 100%;
   min-height: 100vh;
