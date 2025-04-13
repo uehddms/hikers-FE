aff8cc85b92d656457bb27b8e029fc471ef8f81c
ynzung <syj91105@sookmyung.ac.kr>
2025-04-13 20:11:51 +0900

    Feat: 회원가입 페이지 UI 구현

---
+++ src/pages/signup/SignUp.tsx
@@ -0,0 +1,92 @@
+import * as Styled from "./styled";
+import { BottomBtn } from "../../components/common/button/BottomBtn";
+import { Input } from "../../components/common/input/Input";
+import { Header } from "../../components/common/header/Header";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const SignUp = () => {
+  const navigate = useNavigate();
+  const [formValue, setFormValue] = useState({
+    username: "",
+    password: "",
+    nickname: "",
+    email: "",
+  });
+  const handleChange = (e: { target: { name: string; value: string } }) => {
+    const { name, value } = e.target;
+    setFormValue((prevFormValue) => ({
+      ...prevFormValue,
+      [name]: value,
+    }));
+  };
+
+  const isValid =
+    formValue.username.trim() !== "" &&
+    formValue.password.trim() !== "" &&
+    formValue.nickname.trim() !== "" &&
+    formValue.email.trim() !== "";
+
+  const handleBackBtn = () => {
+    navigate("/");
+  };
+
+  const handleSignUp = () => {
+    console.log("회원가입 버튼 클릭");
+  };
+
+  return (
+    <Styled.Wrapper>
+      <Header isOnboarding={true} onClick={handleBackBtn} />
+      <Styled.TitleWrapper>
+        <Styled.Title>회원가입</Styled.Title>
+      </Styled.TitleWrapper>
+      <Styled.InputWrapper>
+        <Styled.InputContent>
+          <Input
+            title="아이디"
+            type="text"
+            name="username"
+            value={formValue.username}
+            onChange={handleChange}
+            placeholder="아이디를 입력해 주세요"
+          />
+          <Input
+            title="비밀번호"
+            type="password"
+            name="password"
+            value={formValue.password}
+            onChange={handleChange}
+            placeholder="비밀번호를 입력해 주세요"
+          />
+        </Styled.InputContent>
+        <Styled.Line></Styled.Line>
+        <Styled.InputContent>
+          <Input
+            title="닉네임"
+            type="text"
+            name="nickname"
+            value={formValue.nickname}
+            onChange={handleChange}
+            placeholder="닉네임을 입력해 주세요"
+          />
+          <Input
+            title="이메일"
+            type="email"
+            name="email"
+            value={formValue.email}
+            onChange={handleChange}
+            placeholder="이메일을 입력해 주세요"
+          />
+        </Styled.InputContent>
+        <Styled.ButtonWrapper>
+          <BottomBtn disabled={!isValid} onClick={handleSignUp}>
+            회원가입하기
+          </BottomBtn>
+        </Styled.ButtonWrapper>
+      </Styled.InputWrapper>
+    </Styled.Wrapper>
+  );
+};
+
+export default SignUp;
---
+++ src/pages/signup/styled.ts
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  min-height: 100vh;
+  padding-bottom: 3.75rem;
+`;
+
+export const TitleWrapper = styled.div`
+  width: 330px;
+  padding: 1.25rem 0 5.06rem 0;
+`;
+
+export const Title = styled.div`
+  display: flex;
+  color: #349989;
+  font-family: "Pretendard";
+  font-size: 1.875rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 2.25rem;
+`;
+
+export const InputContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 1.88rem;
+`;
+
+export const Line = styled.hr`
+  width: 20rem;
+  height: 0.0625rem;
+  background: #c8c8c8;
+  margin: 0;
+  border: none;
+`;
+
+export const ButtonWrapper = styled.div`
+  padding-top: 2.25rem;
+`;
