31a6dfd3ac2e43462212a59a6f3dd4276561d0e7
ynzung <syj91105@sookmyung.ac.kr>
2025-04-13 23:13:05 +0900

    Style: router.tsx 코드 스타일 수정

--- src/router.tsx
+++ src/router.tsx
@@ -3,24 +3,17 @@ import App from "./App";
 import Onboarding from "./pages/onboarding/Onboarding";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
+import Success from "./pages/signup/Success";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      {
-        path: "/",
-        element: <Onboarding />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
-      {
-        path: "/signup",
-        element: <SignUp />,
-      },
+      { path: "/", element: <Onboarding /> },
+      { path: "login", element: <Login /> },
+      { path: "signup", element: <SignUp /> },
+      { path: "signup/success", element: <Success /> },
     ],
   },
 ]);
